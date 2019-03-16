0ec8665c37c281e3baed1ac8fb1d78e465c9926b
Kunal Hire <kunal.hire@hexad.de>
2019-03-16 21:38:30 +0100

    Added login page

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import {withStyles} from '@material-ui/core/styles';
 import withRoot from '../withRoot';
 import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
 
 const styles = theme => ({
     root: {
@@ -36,24 +37,31 @@ class Login extends React.Component {
                     Welcome to Digital Suggestion Box
                 </Typography>
                 <form className={classes.container} noValidate autoComplete="off">
-                    <TextField
-                        id="standard-with-placeholder"
-                        label="Username"
-                        placeholder="Username"
-                        className={classes.textField}
-                        margin="normal"
-                    />
-                    <TextField
-                        id="standard-password-input"
-                        label="Password"
-                        className={classes.textField}
-                        type="password"
-                        autoComplete="current-password"
-                        margin="normal"
-                    />
-                    <Button variant="contained" color="primary" onClick={this.handleClick}>
-                        Login
-                    </Button>
+                    <Grid
+                        container
+                        direction="column"
+                        justify="center"
+                        alignItems="center"
+                    >
+                        <TextField
+                            id="standard-with-placeholder"
+                            label="Username"
+                            placeholder="Username"
+                            className={classes.textField}
+                            margin="normal"
+                        />
+                        <TextField
+                            id="standard-password-input"
+                            label="Password"
+                            className={classes.textField}
+                            type="password"
+                            autoComplete="current-password"
+                            margin="normal"
+                        />
+                        <Button variant="contained" color="secondary" onClick={this.handleClick}>
+                            Login
+                        </Button>
+                    </Grid>
                 </form>
             </div>
         );
