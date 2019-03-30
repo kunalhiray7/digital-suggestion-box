d17fb94c666e5e2dc182f0ba65aacde3d20e858a
Kunal Hire <kunal.hire@hexad.de>
2019-03-30 13:33:24 +0100

    Added left navigation and skeleton for dashboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from "./login/login";
+import Dashboard from "./common/dashboard";
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Login}/>
+            <Route exact path="/dashboard" component={Dashboard}/>
           </Switch>
         </BrowserRouter>
     );
